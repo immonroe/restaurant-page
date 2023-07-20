312d2091b500503dee8d5359237d3d9ac6b79673
immonroe <immanuelmonroe@gmail.com>
2023-07-20 10:39:41 -0400

    Tested to verify that h1 tag appends to div via DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import './style.css';
+
+window.onload=function(){//from  ww w  . j a  va 2s. co m
+    var example = document.getElementById('content');
+    var h1 = document.createElement('h1');
+    h1.innerText = "hi";
+    example.appendChild(h1);
+        }
+
 console.log('test')
